b31bcb8c4c2a9c63f56d6b7b2098ca57282611a3
Apple <huangduduo@hotmail.com>
2025-04-23 15:15:25 +0800

    additional

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -4,20 +4,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 os.chdir("/home/apple/桌面/Learn/IBI1_2024-25/Practical10")
 dalys_data=pd.read_csv("dalys-rate-from-all-causes.csv")
-print ("3rd column:",dalys_data.iloc[0:10,2])
+print ("3rd column:\n",dalys_data.iloc[0:10,2])
 #The 10th year is 1999
 
 Year=dalys_data.loc[:,"Year"]
 Year_1990=[]
 for i in range(len(Year)):
     Year_1990.append(Year[i]==1990)
-print ("All countries in 1990:",dalys_data.loc[Year_1990,"DALYs"])
+In_1990=dalys_data.loc[Year_1990,"DALYs"]
+print ("DALYs of all countries in 1990:\n",In_1990)
 
 uk=dalys_data.loc[dalys_data.Entity=="United Kingdom",["DALYs","Year"]]
 uk_mean=np.mean(uk.DALYs)
 fr=dalys_data.loc[dalys_data.Entity=="France",["DALYs","Year"]]
 fr_mean=np.mean(fr.DALYs)
-print (f'DALYs mean: UK mean={uk_mean}, France mean={fr_mean}')
+print (f'DALYs mean: UK mean={uk_mean}, France mean={fr_mean}\n')
 #Mean DALYs in the UK was geater than France
 
 plt.plot(uk.Year,uk.DALYs,'b+')
@@ -25,4 +26,9 @@ plt.title('The DALYs over time in the UK')
 plt.xlabel('Year')
 plt.ylabel('DALYs')
 plt.xticks(uk.Year,rotation=-90)
+plt.show()
+
+#Additional question: boxplot of 1990
+plt.boxplot(dalys_data.loc[Year_1990,'DALYs'])
+plt.title('Boxplot of DALYs across countries in 1990')
 plt.show()
